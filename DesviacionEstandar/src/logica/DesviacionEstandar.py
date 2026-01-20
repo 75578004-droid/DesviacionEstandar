64d9d9e76a99509bee7ba77354292ddcbed33e9c
12 <MarshaPanda2>
2026-01-19 19:03:02 -0500

    Desviacion estandar implementada correctamente

--- DesviacionEstandar/src/logica/DesviacionEstandar.py
+++ DesviacionEstandar/src/logica/DesviacionEstandar.py
@@ -3,4 +3,13 @@ class DesviacionEstandar:
         self.__datos = datos
 
     def calcular(self):
-        return False
+        if len(self.__datos) == 0:
+            return None
+        
+        if len(self.__datos) == 1:
+            return 0.0
+        
+        promedio = sum(self.__datos) / len(self.__datos)
+        suma_cuadrados = sum((x - promedio) ** 2 for x in self.__datos)
+        varianza = suma_cuadrados / len(self.__datos)
+        return varianza ** 0.5
--- DesviacionEstandar/tests/TestDesviacionEstandar.py
+++ DesviacionEstandar/tests/TestDesviacionEstandar.py
@@ -3,4 +3,22 @@ from src.logica.DesviacionEstandar import DesviacionEstandar
 
 
 class TestDesviacionEstandar(unittest.TestCase):
-    pass
+    def test_conjunto_vacio_retornaNone(self):
+        desviacion = DesviacionEstandar([])
+        self.assertIsNone(desviacion.calcular())
+
+    def test_unElemento_retornaCero(self):
+        desviacion = DesviacionEstandar([5])
+        self.assertEqual(0.0, desviacion.calcular())
+
+    def test_dosElementos_retornaDesviacion(self):
+        desviacion = DesviacionEstandar([2, 4])
+        self.assertEqual(1.0, desviacion.calcular())
+
+    def test_nElementosSinDispersion_retornaCero(self):
+        desviacion = DesviacionEstandar([5, 5, 5, 5])
+        self.assertEqual(0.0, desviacion.calcular())
+
+    def test_nElementosConDispersion_retornaDesviacion(self):
+        desviacion = DesviacionEstandar([2, 4, 6, 8])
+        self.assertAlmostEqual(2.236, desviacion.calcular(), places=3)
